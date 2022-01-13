697c4aee1844fde2f89d8f557c0c09813208ccf7
1Magz1 <46316981+1Magz1@users.noreply.github.com>
2022-01-13 15:53:00 +0700

    add connect wallet

--- components/Header/index.vue
+++ components/Header/index.vue
@@ -1,14 +1,24 @@
 <template>
   <div class="header">
     <button class="btn btn-primary" @click="connectWallet">
-      connect wallet
+      <span v-if="isConnected">{{ userAddress }}</span>
+      <span v-else>Connect Wallet</span>
     </button>
   </div>
 </template>
 
 <script  lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
-@Component
+import { mapGetters } from 'vuex'
+
+@Component({
+  computed: {
+    ...mapGetters('web3', {
+      userAddress: 'getUserAddress',
+      isConnected: 'getIsConnected'
+    })
+  }
+})
 export default class Header extends Vue {
   public connectWallet (): void {
     this.$store.dispatch('web3/connectWallet')
